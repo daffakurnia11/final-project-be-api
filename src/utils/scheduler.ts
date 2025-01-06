305927f0e66bca7e4abc3c1a5ae79b95764dfe45
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2025-01-06 11:27:01 +0700

    fix: Fix cronjob schedule

--- src/utils/scheduler.ts
+++ src/utils/scheduler.ts
@@ -5,12 +5,16 @@ import { startCalculate } from "./calculation";
 cron.schedule(
   "0 0 * * *",
   async () => {
-    await startPredict("Sensor 1");
-    await startCalculate("Sensor 1");
-    await startPredict("Sensor 2");
-    await startCalculate("Sensor 2");
-    await startPredict("Sensor 3");
-    await startCalculate("Sensor 3");
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const yesterdayDate = yesterday.toISOString().split("T")[0];
+
+    await startPredict("Sensor 1", yesterdayDate);
+    await startCalculate("Sensor 1", yesterdayDate);
+    await startPredict("Sensor 2", yesterdayDate);
+    await startCalculate("Sensor 2", yesterdayDate);
+    await startPredict("Sensor 3", yesterdayDate);
+    await startCalculate("Sensor 3", yesterdayDate);
   },
   {
     timezone: "UTC",
